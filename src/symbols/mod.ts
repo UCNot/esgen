994c09b006f261099bbfd1f27cd41bebc356f623
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-05-12 10:03:17 +0700

    Export `EsImport`

--- src/symbols/mod.ts
+++ src/symbols/mod.ts
@@ -1,6 +1,7 @@
 export * from './es-external-module.js';
 export * from './es-import.js';
 export * from './es-imported-symbol.js';
+export * from './es-imports.js';
 export * from './es-module.js';
 export * from './es-namespace.js';
 export * from './es-symbol.js';
